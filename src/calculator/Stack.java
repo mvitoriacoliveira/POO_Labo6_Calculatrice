660da7ba38d55e95d8e7895ba40efdb8c65e0be8
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-11-16 14:43:15 +0100

    implementation of Stack constructors, push, pop methods

--- src/calculator/Stack.java
+++ src/calculator/Stack.java
@@ -1,4 +1,27 @@
 package calculator;
 
 public class Stack<T> {
+    Node<T> top;
+
+    Stack(T e, Stack<T> oldS){
+        this.top = new Node<T>(e, oldS.top);
+    }
+    Stack(T e){
+        new Stack<T>(e, null);
+    }
+
+    void push(T e){
+        this.top = new Node<T>(e, this.top);
+    }
+
+    T pop(){
+        // TODO remplacer par if empty?
+        if(this.top == null){
+            throw new NullPointerException("This stack is empty");
+        }
+        T ret = this.top.data;
+        this.top = this.top.next;
+
+        return ret;
+    }
 }
