4b918b2934377f19ad2e05221f33799497348183
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-11-22 14:44:24 +0100

    format + mini modifs

--- src/calculator/Stack.java
+++ src/calculator/Stack.java
@@ -3,20 +3,21 @@ package calculator;
 public class Stack<T> {
     Node<T> top;
 
-    Stack(T e, Stack<T> oldS){
+    Stack(T e, Stack<T> oldS) {
         this.top = new Node<T>(e, oldS.top);
     }
-    Stack(T e){
+
+    Stack(T e) {
         new Stack<T>(e, null);
     }
 
-    void push(T e){
+    void push(T e) {
         this.top = new Node<T>(e, this.top);
     }
 
-    T pop(){
+    T pop() {
         // TODO remplacer par if empty?
-        if(this.top == null){
+        if (this.top == null) {
             throw new NullPointerException("This stack is empty");
         }
         T ret = this.top.data;
@@ -24,4 +25,10 @@ public class Stack<T> {
 
         return ret;
     }
+
+    /*
+    - obtenir la représentation sous la forme de chaîne de caractères du contenu de la pile ;
+    - obtenir un tableau d’objets représentant l’état actuel de la pile (l’indice 0 contenant l’élément placé au sommet de la pile) ;
+    - obtenir un itérateur sur la pile offrant les opérations T next() et boolean hasNext().
+    */
 }
