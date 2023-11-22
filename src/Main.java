98b695981d54a7bbb2b2b8174140af5036fc7454
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-11-22 18:40:12 +0100

    tests for node, iterator, stack implementation

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
 import calculator.JCalculator;
+import calculator.*; //import Stack, Node, Examinator
+
+import java.util.ArrayList; // for Stack::get.Tab() test
 
 public class Main
 {
   public static void main(String ... args) {
-    new JCalculator();
+    //new JCalculator();
+
+    System.out.println("\n---------START TEST STACK, NODE, ITERATOR IMPLEMENTATION---------");
+
+    Stack<Integer> stack = new Stack<>();
+    ArrayList<Integer> tab = stack.currentState();
+
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    System.out.println("Stack content: " + stack);
+    System.out.println("Stack content as array: " + stack.currentState());
+
+    int poppedItem = stack.pop();
+    System.out.println("Popped item: " + poppedItem);
+    System.out.println("Stack content after pop: " + stack);
+
+    Iterator<Integer> iterator = stack.iterator();
+    System.out.println("Using iterator to examine the stack:");
+    while (iterator.hasNext()) {
+      int item = iterator.next();
+      System.out.println("Examined item: " + item);
+    }
+    System.out.println("Stack content: " + stack);
+
+
+
+    System.out.println("\n--------- END---------");
+
   }
 }
