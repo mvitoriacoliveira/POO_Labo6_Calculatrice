87f07f5e7aa1d07291dbb2c30c21ab1ce839abdd
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-11-29 16:43:04 +0100

    state implementation

--- src/calculator/State.java
+++ src/calculator/State.java
@@ -1,4 +1,23 @@
 package calculator;
 
 public class State {
+    private Stack<Double> stack = new Stack<Double>();
+    private Double currentInput;
+
+    public Stack<Double> getStack(){
+        return this.stack;
+    }
+
+    public Double getCurrentInput(){
+        Double ret = this.currentInput;
+        stack.pop();
+        //mettre this.currentInput = derniere valeur de la stack? TODO
+        return ret;
+    }
+
+    public void setCurrentInput(Double currentInput){
+        this.currentInput = currentInput;
+        stack.push(currentInput);
+    }
+
 }
