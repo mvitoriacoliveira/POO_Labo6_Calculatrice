7f6d02855bd18843c3dd83b714b7ca848d057a53
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-12-01 15:42:23 +0100

    sqrt op

--- src/calculator/Operation/SquareRoot.java
+++ src/calculator/Operation/SquareRoot.java
@@ -1,21 +1,42 @@
 package calculator.Operation;
+
 import calculator.State;
+import calculator.Formatter;
 
-
+/**
+ * The {@code SquareRoot} class represents the square root operation (√x).
+ * It extends the {@code Operator} class and implements the {@code execute} method.
+ * The result is formatted using the provided {@code Formatter}.
+ */
 public class SquareRoot extends Operator {
 
-    public SquareRoot(State s){
+    /**
+     * Constructs a {@code SquareRoot} object with the given state.
+     *
+     * @param s The state of the calculator.
+     */
+    public SquareRoot(State s) {
         super(s);
     }
+
+    /**
+     * Executes the square root operation (√x).
+     * If the current input is non-negative, calculates the result and updates the state.
+     * Otherwise, throws an {@code ArithmeticException} with the message "Cannot take the square root of a negative number."
+     *
+     * @throws ArithmeticException If attempting to take the square root of a negative number.
+     */
+    @Override
     public void execute() {
         double input = Double.parseDouble(state.getCurrentInput());
+
         if (input >= 0) {
-            state.setCurrentInput(String.valueOf(Math.sqrt(input)));
+            double result = Math.sqrt(input);
+            String formattedResult = Formatter.format(result);
+            state.setCurrentInput(formattedResult);
         } else {
             throw new ArithmeticException("Cannot take the square root of a negative number.");
         }
     }
-
-
 }
 
