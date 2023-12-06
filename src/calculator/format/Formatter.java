d80f7c2869f2fd55b77258fa79f53ebaba6ffc18
mvitoriacoliveira <mvitoriacoliveira@gmail.com>
2023-12-06 11:46:31 +0100

    add headers for format classes

--- src/calculator/format/Formatter.java
+++ src/calculator/format/Formatter.java
@@ -5,34 +5,33 @@ import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
 /**
- * The {@code Formatter} class provides a utility method to format double results.
- * It uses DecimalFormat to format the double result based on a specified pattern.
- * Adjust the pattern in the {@code DECIMAL_PATTERN} constant as needed.
+ * The Formatter class provides methods for formatting numerical values.
+ * It includes functionality for formatting double values, ensuring a specific decimal pattern.
+ * Additionally, it supports formatting values with one decimal place and checks whether a given string is numeric.
  *
- *  @author Vitoria Oliveira
- *  @author Camille Koestli
- *  @since 05.12.23
+ * @author Vitoria Oliveira
+ * @since 05.12.23
  */
 public class Formatter {
-    /**
-     * The decimal pattern used for formatting double results.
-     * Adjust this pattern as needed.
-     */
+    /* The default decimal pattern for formatting double values. */
     private static final String DECIMAL_PATTERN = "#.##########";
 
     /**
-     * Formats a double result using the specified decimal pattern.
-     * Up to 10 decimal places will be shown if they are present in the number.
-     * If there are fewer decimal places, only the necessary ones will be displayed.
-     *
+     * Formats a double value using the default decimal pattern.
      * @param result The double value to be formatted.
-     * @return A formatted string representing the double result.
+     * @return A formatted string representation of the double value.
      */
     public static String format(double result) {
         DecimalFormat decimalFormat = new DecimalFormat(DECIMAL_PATTERN, DecimalFormatSymbols.getInstance(Locale.US));
         return decimalFormat.format(result);
     }
 
+    /**
+     * Formats a string representing a numeric value with one decimal place.
+     * If the input is not a numeric value, it is returned unchanged.
+     * @param input The string representing the numeric value.
+     * @return A formatted string with one decimal place, or the original input if it is not numeric.
+     */
     public static String formatOneDecimal(String input) {
         try {
             if ( isNumeric(input) && !(input.contains(".")) && !(input.contains(","))) {
@@ -49,6 +48,11 @@ public class Formatter {
         }
     }
 
+    /**
+     * Checks if a given string is numeric.
+     * @param str The string to be checked.
+     * @return true if the string is numeric, false otherwise.
+     */
     public static boolean isNumeric(String str) {
         try {
             Double.parseDouble(str);
